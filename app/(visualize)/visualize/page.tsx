5390da6200e0c16fcfad78c22823250c15f97dc7
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-05-10 07:13:15 -0400

    Additions: - AG-Grid Added. - Project ready to pickup later starting with columns

--- app/(visualize)/visualize/page.tsx
+++ app/(visualize)/visualize/page.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
 const VisualizePage = () => {
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
   return (
-    <article className="w-full p-4 text-center border border-gray-200 rounded-lg shadow sm:p-8 bg-gray-800 dark:border-gray-700 max-w-5xl">
-      VisualizePage
-    </article>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    </div>
   );
 };
 
